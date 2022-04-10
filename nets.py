3a78fcabccaa87e8ff9a9124c99cb73b2618d64f
Blathers <nicbatte@qq.com>
2022-04-10 17:39:10 +0800

    first commit

---
+++ nets.py
@@ -0,0 +1,372 @@
+import torch 
+import torch.nn as nn 
+
+class ConvGRUCell(nn.Module):
+    r"""
+    初始化 ConvGRU cell
+
+    Parameters:
+    -----------
+    input_dim: int
+        输入tensor的channel
+    hidden_dim: int
+        隐状态的channel
+    kernel_size: int
+        卷积核的size
+    bias: bool
+        是否添加bias
+    """
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        super().__init__()
+        self.input_dim = input_dim 
+        self.hidden_dim = hidden_dim
+        
+        self.kernel_size = kernel_size
+        self.padding = 'same'
+        self.bias = bias
+
+        self.conv1 = nn.Conv2d(in_channels=self.input_dim+self.hidden_dim,
+                                out_channels=2*self.hidden_dim,
+                                kernel_size=self.kernel_size,
+                                padding=self.padding,
+                                bias=self.bias)
+        self.conv2 = nn.Conv2d(in_channels=input_dim+self.hidden_dim,
+                                out_channels=self.hidden_dim,
+                                kernel_size=self.kernel_size,
+                                padding=self.padding,
+                                bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur = cur_state
+
+        combined1 = torch.cat([input_tensor, h_cur], dim=1)  #concat alone channel axis
+        
+        combined_conv = self.conv1(combined1)
+        cc_r, cc_z = torch.split(combined_conv, self.hidden_dim, dim=1)
+        r = torch.sigmoid(cc_r)
+        z = torch.sigmoid(cc_z)
+
+        combined2 = torch.cat([input_tensor, r * h_cur], dim=1)
+        h_next_ = self.conv2(combined2)
+
+        h_next = z * h_cur + (torch.ones_like(z) - z) * h_next_
+
+        return h_next      
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv1.weight.device)
+
+class ConvGRU(nn.Module):
+    r"""
+    Parameters:
+    -------------
+    input_dim: int
+        输入channel
+    hidden_dim: int or list
+        隐状态channel
+    kernel_size: int
+        卷积核size
+    num_layers: int
+        LSTM层的数量
+    batch_first: bool
+        输入数据的第0维是否是batch
+    bias: bool 
+        卷积中是否加入偏置
+
+    Input:
+    -------------
+        (b, t, c, h, w) or (t, b, c, h, w)
+
+    Output:
+    -------------
+        预测结果: (b, h, w)
+        
+    Example:
+    -------------
+        >> x = torch.rand((32, 10, 64, 128, 128)) 
+
+        >> convgru = ConvGRU(64, 16, 3, 3, 3, True, True) 
+
+        >> pred = convgru(x) 
+    """ 
+    def __init__(self, input_dim, hidden_dim, kernel_size, dec_kernel_size, num_layers,
+                batch_first=False, bias=True):
+        super().__init__()
+
+        self._check_kernel_size_consistency(kernel_size)
+
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
+        if not len(kernel_size) == len(hidden_dim) == num_layers:
+            raise ValueError('Inconsistent list length.')
+        
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim 
+        self.kernel_size = kernel_size 
+        self.dec_kernel_size = dec_kernel_size
+        self.num_layers = num_layers 
+        self.batch_first = batch_first
+        self.bias = bias 
+
+        self.padding = 'same'
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+
+            cell_list.append(ConvGRUCell(input_dim=cur_input_dim, 
+                                hidden_dim=self.hidden_dim[i],
+                                kernel_size=self.kernel_size[i],
+                                bias=self.bias))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+        self.dec = nn.Conv2d(in_channels=self.hidden_dim[-1]+1,
+                            out_channels=1, kernel_size=self.dec_kernel_size, 
+                            padding=self.padding, bias=self.bias)
+    
+    def forward(self, input_tensor, hidden_state=None):
+        r"""
+        Parameters:
+        -----------
+        input_tensor: 
+            5-D tensor (t, b, c, h, w) or (b, t, c, h, w)
+        hidden_state:
+            None
+        
+        Returns:
+        ----------
+        last_state_list, layer_out
+        """
+        if not self.batch_first:
+            # (t, b, c, h, w) -> (b, t, c, h, w)
+            input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+
+        b, _, _, h, w = input_tensor.size()
+
+        #Implement stateful ConvGRU
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            hidden_state = self._init_hidden(batch_size=b, image_size=(h, w))
+
+        seq_len = input_tensor.size(1)
+        cur_layer_input = input_tensor
+
+        for layer_idx in range(self.num_layers):
+
+            h = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
+                                                    cur_state=h)
+                output_inner.append(h)
+            
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+        pred = self.dec(torch.cat([input_tensor[:, -1, 0, :, :].unsqueeze(dim=1), layer_output[:, -1, :, :, :]], dim=1))
+
+        return pred[:, 0, :, :]
+
+    def _init_hidden(self, batch_size, image_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, image_size))
+        return init_states
+
+    @staticmethod   #静态方法无需实例化
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or isinstance(kernel_size, int) or (isinstance(kernel_size, list)
+            and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('kernel_size must be int or tuple or list of tuples')
+    
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
+
+class ConvLSTMCell(nn.Module):
+    r"""
+    初始化 ConvLstm cell
+
+    Parameters:
+    -----------
+    input_dim: int
+        输入tensor的channel
+    hidden_dim: int
+        隐状态的channel
+    kernel_size: int
+        卷积核的size
+    bias: bool
+        是否添加bias
+    """
+
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        super().__init__()
+        self.input_dim = input_dim 
+        self.hidden_dim = hidden_dim
+        
+        self.kernel_size = kernel_size
+        self.padding = 'same'
+        self.bias = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim+self.hidden_dim,
+                                out_channels=4*self.hidden_dim,
+                                kernel_size=self.kernel_size,
+                                padding=self.padding,
+                                bias=self.bias)
+        
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)  #concat alone channel axis
+        
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
+
+class ConvLSTM(nn.Module):
+    r"""
+    Parameters:
+    -------------
+    input_dim: int
+        输入channel
+    hidden_dim: int
+        隐状态channel
+    kernel_size: int
+        卷积核size
+    num_layers: int
+        LSTM层的数量
+    batch_first: bool
+        输入数据的第0维是否是batch
+    bias: bool 
+        卷积中是否加入偏置
+
+    Input:
+    -------------
+        (b, t, c, h, w) or (t, b, c, h, w)
+
+    Output:
+    -------------
+        预测结果: (b, h, w)
+        
+    Example:
+    -------------
+        >> x = torch.rand((32, 10, 64, 128, 128)) 
+
+        >> convlstm = ConvLSTM(64, 16, 3, 3, 1, True, True, True) 
+
+        >>pred = convlstm(x) 
+    """ 
+    def __init__(self, input_dim, hidden_dim, kernel_size, dec_kernel_size, num_layers,
+                batch_first=False, bias=True):
+        super().__init__()
+
+        self._check_kernel_size_consistency(kernel_size)
+
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
+        if not len(kernel_size) == len(hidden_dim) == num_layers:
+            raise ValueError('Inconsistent list length.')
+        
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim 
+        self.kernel_size = kernel_size 
+        self.dec_kernel_size = dec_kernel_size
+        self.num_layers = num_layers 
+        self.batch_first = batch_first
+        self.bias = bias 
+        self.padding = 'same'
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+
+            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim, 
+                                hidden_dim=self.hidden_dim[i],
+                                kernel_size=self.kernel_size[i],
+                                bias=self.bias))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+        self.dec = nn.Conv2d(in_channels=self.hidden_dim[-1]+1,
+                            out_channels=1, kernel_size=self.dec_kernel_size, 
+                            padding=self.padding, bias=self.bias)
+    
+    def forward(self, input_tensor, hidden_state=None):
+        r"""
+        Parameters:
+        -----------
+        input_tensor: 
+            5-D tensor (t, b, c, h, w) or (b, t, c, h, w)
+        hidden_state:
+            None
+        
+        Returns:
+        ----------
+        last_state_list, layer_out
+        """
+        if not self.batch_first:
+            # (t, b, c, h, w) -> (b, t, c, h, w)
+            input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+
+        b, _, _, h, w = input_tensor.size()
+
+        #Implement stateful ConvLSTM
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            hidden_state = self._init_hidden(batch_size=b, image_size=(h, w))
+
+        seq_len = input_tensor.size(1)
+        cur_layer_input = input_tensor
+
+        for layer_idx in range(self.num_layers):
+
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
+                                                    cur_state=[h, c])
+                output_inner.append(h)
+            
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+        pred = self.dec(torch.cat([input_tensor[:, -1, 0, :, :].unsqueeze(dim=1), layer_output[:, -1, :, :, :]], dim=1))
+
+        return pred[:, 0, :, :]
+
+    def _init_hidden(self, batch_size, image_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, image_size))
+        return init_states
+
+    @staticmethod   #静态方法无需实例化
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or isinstance(kernel_size, int) or (isinstance(kernel_size, list)
+            and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('kernel_size must be int or tuple or list of tuples')
+    
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
---
+++ tools.py
@@ -0,0 +1,194 @@
+from mimetypes import init
+import numpy as np
+import torch 
+from torch import nn
+import time 
+import random
+import numpy as np 
+from IPython import display
+from matplotlib import pyplot as plt
+
+def try_gpu(i=0):
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter."""
+    display.set_matplotlib_formats('svg')
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib"""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+class Timer:
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+    
+    def stop(self):
+        """Stop the timer and record the time in a list"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average of time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.tiems)
+
+    def cumsum(self):
+        """return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+class Accumulator:
+    """For accumulating sums over 'n' variables"""
+    def __init__(self, n):
+        self.data = [0.] * n
+    
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+    
+    def reset(self):
+        self.data = [0.] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+class Animator:
+    """For plotting data in animation """
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                ylim=None, xscale='linear', yscale='linear', 
+                fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                figsize=(3.5, 2.5)):
+        if legend is None:
+            legend = []
+        use_svg_display()
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes,]
+        # Use a lambda function to capture arguments
+        self.config_axes = lambda: set_axes(self.axes[0], xlabel, ylabel, 
+        xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # Add multiple data points into the figure
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+def seq_data_iter_random(pictures, batch_size, num_steps):
+    """使用随机抽样生成一个小批量子序列"""
+    # 从随机偏移量开始对序列进行分区，随即范围包括num_stpes-1
+    pictures = pictures[random.randint(0, num_steps - 1)]
+    # 由于要考虑标签，序列长度减一
+    num_subseqs = (len(pictures) - 1) // num_steps
+    # 长度为num_steps的子序列的起始索引
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    # 在迭代抽样的过程中，
+    # 来自两个相邻的、随机的、小批量的子序列不一定在原始序列上相邻
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        # 返回从pos位置开始的长度为num_steps的序列
+        return pictures[pos: pos + num_steps]
+    
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        # initial_indices 包含子序列的随机起始索引
+        initial_indices_per_batch = initial_indices[i: i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(np.array(X)), torch.tensor(np.array(Y))
+
+def predict(net, data, device):
+    raise NotImplementedError()
+    # data = data.to(device)
+    # y_hat = net(data)
+
+def grad_clipping(net, theta):
+    """Clip the gradient"""
+    params = [p for p in net.parameters() if p.requires_grad]
+    norm = torch.sqrt(sum(torch.sum((p.grad**2))for p in params))
+    if norm > theta:
+        for param in params:
+            param.gard[:] *= theta / norm
+
+def train_epoch(net, train_iter, loss, updater, device, use_random_iter=True):
+    """Train a net within one epoch"""
+    state, timer = None, Timer()
+    metric = Accumulator(2) # sum of train loss
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            state = None
+        else:
+            # state.detach_()
+            raise NotImplementedError()
+        X, y = X.to(device), Y.to(device)
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y)
+        updater.zero_grad()
+        l.backward()
+        grad_clipping(net, 1)
+        updater.step()
+        metric.add(l * y.shape[0] * y.shape[1], y.shape[0] * y.shape[1])
+    return metric[0] / metric[1], metric[1] / timer.stop()
+
+def train(net, train_iter, lr, num_epochs, device, use_random_iter=True):
+    """Train a model"""
+    loss = nn.MSELoss()
+    animator = Animator(xlabel='epoch', ylabel='MSE', legend=['train'], xlim=[10, num_epochs])
+    # Initialize
+    updater = torch.optim.SGD(net.parameters(), lr)
+    # train and predict
+    for epoch in range(num_epochs):
+        MSE, speed = train_epoch(net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            animator.add(epoch + 1, [MSE])
+    print(f'MSE {MSE:.2f}, {speed:.1f} pictures/sec on {str(device)}')
+
+def evaluate(net, data_iter, device):
+    net.eval()
+    metric = Accumulator(2)
+
+    with torch.no_grad():
+        loss = nn.MSELoss()
+        for X, y in data_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat, _ = net(X)
+            l = loss(y, y_hat)
+            metric.add(l * y.shape[0] * y.shape[1], y.shape[0] * y.shape[1])
+    return metric[0] / metric[1]
